e9bffd4a1b0300c54250e72278a357eb7a75d75e
Osniel Cebrero Arcos <92766539+OsnielC@users.noreply.github.com>
2023-08-17 23:51:50 -0600

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    FIREBASE_CONFIGURATION
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
 
 const url = "http://testingserver.com.mx/tasks/";
 const application = new function(){
